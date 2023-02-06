7e70473db96a0879b7f9eafce99d71d330c11740
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-06 10:53:50 +0900

    7-1まで終了

---
+++ apperrors/error.go
@@ -0,0 +1,19 @@
+package apperrors
+
+type MyAppError struct {
+	// ErrCode型のErrCodeフィールド
+	// (フィールド名を省略した場合、型名がそのままフィールド名になる)
+	ErrCode        // レスポンスとログに表示するエラーコード
+	Message string // レスポンスに表示するエラーメッセージ
+	Err     error  // エラーチェーンのための内部エラー
+}
+
+// Errorインターフェースを満たすように実装
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+// エラーチェーンにて、Unwrapメソッドを用いるための実装
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
---
+++ apperrors/errorcode.go
@@ -0,0 +1,8 @@
+package apperrors
+
+// 独自型で定義
+type ErrCode string
+
+const (
+	Unknown ErrCode = "U000"
+)
