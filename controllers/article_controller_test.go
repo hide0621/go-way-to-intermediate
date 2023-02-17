2deb3bbe6a9712b2647c77e9e9d3171a4951a985
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-17 09:02:57 +0900

    8-1全て終了

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -19,7 +19,7 @@ func TestArticleListHandler(t *testing.T) {
 		{name: "alphabet query", query: "aaa", resultCode: http.StatusBadRequest},
 	}
 
-	for tt, _ := range tests {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			url := fmt.Sprintf("http://localhost:8080/article/list?page=%s", tt.query)
 			req := httptest.NewRequest(http.MethodGet, url, nil)
@@ -28,9 +28,6 @@ func TestArticleListHandler(t *testing.T) {
 
 			aCon.ArticleListHandler(res, req)
 
-			if [期待していたhttpレスポンスコードが得られたかどうか] {
-				t.Error("Unexpected StatusCode")
-			}
 			if res.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
 			}
