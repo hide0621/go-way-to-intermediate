8cdfb0242b8dbc103c9c93d6673d01a87cc3467c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-01 13:31:11 +0900

    9-2まで終了

---
+++ api/middlewares/logging.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// リクエスト情報をロギング
+		log.Println(req.RequestURI, req.Method)
+
+		next.ServeHTTP(w, req)
+	})
+}
--- api/router.go
+++ api/router.go
@@ -3,6 +3,7 @@ package api
 //別パッケージで定義したものを使うには「モジュール（プロジェクト）名/対象のディレクトリ(パッケージ)名」
 import (
 	"database/sql"
+	"go-way-to-intermediate/api/middlewares"
 	"go-way-to-intermediate/controllers"
 	"go-way-to-intermediate/services"
 	"net/http"
@@ -19,13 +20,15 @@ func NewRouter(db *sql.DB) *mux.Router {
 	// 明示的にルーターを使うことを宣言
 	r := mux.NewRouter()
 
-	//ここでHTTPメソッドと紐づけることが出来て、デフォルトで405エラーも返してくれる
+	// ここでHTTPメソッドと紐づけることが出来て、デフォルトで405エラーも返してくれる
 	r.HandleFunc("/hello", aCon.HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article", aCon.PostArticleHandler).Methods(http.MethodPost)
 	r.HandleFunc("/article/list", aCon.ArticleListHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet) //パスパラメータで定義してハンドラ関数でそれを利用する
 	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
+
+	r.Use(middlewares.LoggingMiddleware)
 
 	return r
 
