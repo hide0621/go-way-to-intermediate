e19fe4a28f7f900464ea61a0231abaf0426f6060
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-01 13:32:49 +0900

    エラーの修正

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -7,7 +7,7 @@ import (
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
 
